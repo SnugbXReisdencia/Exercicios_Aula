98b50a345debd50eb25a7550eec81914c69d7f38
SnugbXResdencia <santoseverlan29@gmail.com>
2023-08-29 11:38:24 -0300

    add exe Operadores_atribuicao

---
+++ Operadores_atribuicao.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    char op, ch;
+
+    int i = 0, j, k;
+
+    double x,y,z;
+
+    bool isOK, isNotOk;
+
+    op = 's';
+    ch = 78;
+
+    j = k = i;
+
+    x = 0.33333;
+    y = 1.0/3.0;
+    z = 3.33333e-1;
+
+    isOK = true;
+    isNotOk = false;
+
+    return 0;
+}
