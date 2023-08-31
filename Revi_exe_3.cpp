746a1b608ad55719c0a7eb579ba2954b957f690f
SnugbXResdencia <santoseverlan29@gmail.com>
2023-08-31 11:34:43 -0300

    add codigo novo

--- Revi_exe_3.cpp
+++ Revi_exe_3.cpp
@@ -57,5 +57,31 @@ int main(void)
 
     // cout << ch1 << ch2 << ch3 << endl;
 
+    int num1, num2, res=0, i=0, s, s1 = 1, s2 = 1;
+
+    cout << "Digite um numero: ";
+    cin >> num1;
+
+    cout << "Digite outro numero: ";
+    cin >> num2;
+    
+    if(num1 < 0){
+        num1 = -num1;
+        s1 = -1;
+    }
+
+    if(num2 < 0){
+        num2 = -num2;
+        s2 = -1;
+    }
+    s = s1 * s2;
+    while(i< num2){
+        res += num1;
+        i++;
+    }
+
+    cout << "Resultado " << res*s << endl;
+
+
     return 0;
 }
