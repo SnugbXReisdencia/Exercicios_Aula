137ce200ae3fab5d960fee7b0521e236ef662e1c
SnugbXResdencia <santoseverlan29@gmail.com>
2023-09-05 12:24:41 -0300

    add att sala 5/9/23

---
+++ media_Academica.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    vector<int> aluno;
+    vector<vector<int>> boletin;
+    char opc;
+    int valor;
+
+    do
+    {
+        system("clear");
+        
+        cout << "Digite a matricula: ";
+        cin >> valor;
+        aluno.insert(aluno.begin(),valor);
+
+        cout << "Digite a primeira nota: ";
+        cin >> valor;
+        
+        aluno.insert(aluno.begin()+1,valor);
+        cout << "Digite a segunda nota: ";
+        cin >> valor;
+        aluno.insert(aluno.begin()+2,valor);
+
+        cout << "Digite a terceira nota: ";
+        cin >> valor;
+        aluno.insert(aluno.begin()+3,valor);
+
+        boletin.push_back(aluno);
+
+        cout << "Deseja continuar? [S/N]: ";
+        cin >> opc;
+
+    } while (opc != 'N' && opc != 'n');
+    
+    for (int i = 0; i < boletin.size(); i++){
+        cout << "Matricula  Nota1   Nota2   Nota3   Media" << endl;
+        cout << "================================================" << endl;
+        cout << boletin[i][0] << "   " << boletin[i][1] << "   " << boletin[i][2] << "   " << boletin[i][3] << "   " << (boletin[i][1] + boletin[i][2] + boletin[i][3]) / 3 << endl;
+    }
+    
+    return 0;
+}
