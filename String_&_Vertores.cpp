f06c86361924efa36bdd13f201e7eea8486fb5bf
SnugbXResdencia <santoseverlan29@gmail.com>
2023-09-08 11:32:38 -0300

    ATT Aula 8/set/23

--- String_&_Vertores.cpp
+++ String_&_Vertores.cpp
@@ -6,22 +6,49 @@ using namespace std;
 
 int main(void)
 {
-    system("clear");
-    string nome;
-    vector<string> vc;
+    // system("clear");
+    vector<int> vet;
+    int valor, soma = 0;
+    char opc;
 
-    vc.push_back("Zangado");
-    vc.push_back("Feliz");
-    vc.push_back("Atchim");
-    vc.push_back("Mestre");
-    vc.push_back("Soneca");
-    vc.push_back("Dengoso");
-    vc.push_back("Dunga");
-
-    for (auto x = vc.begin(); ; i++)
+    do
     {
-        /* code */
+        system("clear");
+        cout << "Digite um valor: ";
+        cin >> valor;
+        vet.push_back(valor);
+        soma += valor;
+        cout << "Deseja adicionar mais valor (S/N): ";
+        cin >> opc;
+    } while (opc != 'n' && opc != 'N');
+    
+    cout << "{";
+    
+    for (int i : vet){
+        cout << i << ", ";
     }
+    
+    cout << "} " << endl;
+
+    cout << "A media dos valores é: " << soma/vet.size() << endl;
+
+    return 0;
+    
+    // string nome;
+    // vector<string> vc;
+
+    // vc.push_back("Zangado");
+    // vc.push_back("Feliz");
+    // vc.push_back("Atchim");
+    // vc.push_back("Mestre");
+    // vc.push_back("Soneca");
+    // vc.push_back("Dengoso");
+    // vc.push_back("Dunga");
+
+    // for (auto x = vc.begin(); ; i++)
+    // {
+    //     /* code */
+    // }
     
 
 
