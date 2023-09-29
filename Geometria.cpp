6c500e1be58cd141891561702fc71e1cc4e34977
SnugbXResdencia <santoseverlan29@gmail.com>
2023-09-29 09:30:52 -0300

    add mudanças em Geometria.cpp

--- Geometria.cpp
+++ Geometria.cpp
@@ -2,38 +2,73 @@
 # include <limits>
 # include <cstring>
 # include <vector>
+# include <cmath>
 
 using namespace std;
 
 class Ponto{
     private:
-        double x;
-        double y;
+        float x;
+        float y;
     public:
-        Ponto(){
-            x = 0;
-            y = 0;
-        }
-        Ponto(double _x, double _y){
-            x = _x;
-            y = _y;
-        }
-        double getX(){
-            return x;
-        }
-        double getY(){
-            return y;
-        }
-        void setX(double x){
-            this->x = x;
-        }
-        void setY(double y){
-            this->y = y;
-        }
-        string toString(){
-            return "(X:" + to_string(x) + ", Y:" + to_string(y) + ")";
-        }
+        Ponto();
+        Ponto(float _x, float _y);
+
+        float getX();
+        void setX(float x);
+
+        float getY();
+        void sety(float y);
+
+        void le_ponto();
+
+        float distancia(Ponto p);
+
+        string toString();
 };
+
+Ponto::Ponto(){
+    x = 0;
+    y = 0;
+}
+
+Ponto::Ponto(float _x, float _y){
+    x = _x;
+    y = _y;
+}
+
+float Ponto::getX(){
+    return x;
+}
+
+void Ponto::setX(float _x){
+    x = _x;
+}
+
+float Ponto::getY(){
+    return y;
+}
+
+void Ponto::sety(float _y){
+    y = _y;
+}
+
+void Ponto::le_ponto(){
+    cout << "Informe as coordenadas do ponto: " << endl;
+    cout << "Valor de X: ";
+    cin >> x;
+    cout << "Valor de Y: ";
+    cin >> y;
+}
+
+float Ponto::distancia(Ponto p) {
+    return sqrtf((pow((p.getX() - this->getX()),2)) + (pow((p.getY() - this->getY()),2)));
+}
+
+string Ponto::toString(){
+    return "(X:" + to_string(x) + ", Y:" + to_string(y) + ")";
+}
+
 
 class Poligono{
     private:
@@ -47,6 +82,10 @@ class Poligono{
         void setPontos(vector<Ponto> pontos);
 
         void addPonto(Ponto p);
+
+        void le_addPonto();
+
+        float perimetro();
 
         string toString();
 };
@@ -67,6 +106,38 @@ void Poligono::setPontos(vector<Ponto> pontos){
 
 void Poligono::addPonto(Ponto p){
     this->pontos.push_back(p);
+}
+
+void Poligono::le_addPonto(){
+    cout << "Criando um poligono!!" << endl;
+    char sn;
+    do{
+        Ponto p;
+        p.le_ponto();
+        pontos.push_back(p);
+        cout << "Deseja inserir outro ponto(s/n)?" << endl;
+        cin >> sn;
+    }while (sn != 'n');
+}
+
+float Poligono::perimetro(){
+    float per = 0;
+    vector<Ponto>::iterator it2;
+    Ponto p1, p2;
+
+    for (auto it = pontos.begin(); it != pontos.end()-1; it++){
+        it2 = it;
+        advance(it2,1);
+        p1 = *it;
+        p2 = *it2;
+        per += p1.distancia(p2);
+    }
+
+    it2 = pontos.begin();
+    p1 = *it2;
+    per += p1.distancia(p2);
+    
+    return per;
 }
 
 string Poligono::toString(){
